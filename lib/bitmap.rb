e49accb3b85bd0fc251d07a643f00c12459a607f
Wayne Lu <waynelu24lon@gmail.com>
2017-11-19 11:06:38 +0000

    Update Bitmap class
    Update Bitmap class error handling. Also change Bitmap to use
    zero-based indexing. Let BitmapEditor handle user entered
    indices and convert them to zero-based indexing when interacting
    with Bitmap.

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -1,29 +1,23 @@
 # Bitmaps are represented as an M x N matrix of pixels with each
 # element representing a colour.
 class Bitmap
-  MIN_COL = 1
-  MIN_ROW = 1
 
-  attr_reader :max_col, :max_row
+  attr_reader :col_size, :row_size
 
   def initialize(col_size, row_size)
     if col_size < 1 || row_size < 1
       raise ArgumentError, 'column size and row size must be greate than 0'
     end
 
-    @max_col = col_size
-    @max_row = row_size
+    @col_size = col_size
+    @row_size = row_size
 
     @pixel_2d_array = []
     row_size.times { @pixel_2d_array.push(Array.new(col_size, 'O')) }
   end
 
   def set_pixel_color(col1, col2, row1, row2, color)
-    if !valid_column_range?(col1, col2) || !valid_row_range?(row1, row2)
-      error_msg = "column must be within #{MIN_COL} and #{@max_col}; "
-      error_msg += "row must be within #{MIN_ROW} and #{@max_row}"
-      raise ArgumentError, error_msg
-    end
+    validate_coordinates(col1, col2, row1, row2)
 
     cols = [col1, col2].sort
     rows = [row1, row2].sort
@@ -35,12 +29,13 @@ class Bitmap
 
     col_start.upto(col_end) do |col|
       row_start.upto(row_end) do |row|
-        @pixel_2d_array[row - 1][col - 1] = color
+        @pixel_2d_array[row][col] = color
       end
     end
   end
 
   def get_pixel_color(col, row)
+    validate_coordinates(col, col, row, row)
     @pixel_2d_array[row][col]
   end
 
@@ -57,10 +52,18 @@ class Bitmap
   end
 
   def valid_column?(col)
-    MIN_COL <= col && col <= @max_col
+    0 <= col && col < @col_size
   end
 
   def valid_row?(row)
-    MIN_ROW <= row && row <= @max_row
+    0 <= row && row < @row_size
+  end
+
+  def validate_coordinates(col1, col2, row1, row2)
+    if !valid_column_range?(col1, col2) || !valid_row_range?(row1, row2)
+      error_msg = "column must be within 0 and #{@col_size - 1}; "
+      error_msg += "row must be within 0 and #{@row_size - 1}"
+      raise ArgumentError, error_msg
+    end
   end
 end
--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -20,10 +20,10 @@ RSpec.describe 'Bitmap' do
     context 'when argument is valid (3,4)' do
       bitmap = Bitmap.new(3, 4)
       it 'sets @max_col to 3' do
-        expect(bitmap.max_col).to eq(3)
+        expect(bitmap.col_size).to eq(3)
       end
       it 'sets @max_row to 4' do
-        expect(bitmap.max_row).to eq(4)
+        expect(bitmap.row_size).to eq(4)
       end
       it 'set pixel_2d_array to a 2d array of the right size' do
         pixel_2d_array = bitmap.to_a
@@ -40,37 +40,35 @@ RSpec.describe 'Bitmap' do
   describe '#valid_column?' do
     bitmap = Bitmap.new(4, 5)
     it 'returns true when the argument is within column boundaries' do
-      expect(bitmap.valid_column?(1)).to be true
-      expect(bitmap.valid_column?(4)).to be true
+      expect(bitmap.valid_column?(0)).to be true
+      expect(bitmap.valid_column?(3)).to be true
     end
-    it "returns false when the argument is less than column's lower bound (1)" do
-      expect(bitmap.valid_column?(0)).to be false
+    it "returns false when the argument is less than column's lower bound (0)" do
       expect(bitmap.valid_column?(-1)).to be false
     end
-    it "returns false when the argument is greater than column's upper bound (4)" do
-      expect(bitmap.valid_column?(5)).to be false
+    it "returns false when the argument is greater than column's upper bound (3)" do
+      expect(bitmap.valid_column?(4)).to be false
     end
   end
 
   describe '#valid_row?' do
     bitmap = Bitmap.new(5, 6)
     it 'returns true when the argument is within row boundaries' do
-      expect(bitmap.valid_row?(1)).to be true
-      expect(bitmap.valid_row?(6)).to be true
+      expect(bitmap.valid_row?(0)).to be true
+      expect(bitmap.valid_row?(5)).to be true
     end
-    it "returns false when the argument is less than row's lower bound (1)" do
-      expect(bitmap.valid_row?(0)).to be false
+    it "returns false when the argument is less than row's lower bound (0)" do
       expect(bitmap.valid_row?(-1)).to be false
     end
-    it "returns false when the argument is greater than row's upper bound(6)" do
-      expect(bitmap.valid_row?(7)).to be false
+    it "returns false when the argument is greater than row's upper bound(5)" do
+      expect(bitmap.valid_row?(6)).to be false
     end
   end
 
   describe '#valid_column_range?' do
     bitmap = Bitmap.new(6, 7)
     context 'when the given range is within bounds' do
-      valid_ranges = [[1, 6], [6, 1], [3, 6], [5, 1]]
+      valid_ranges = [[0, 5], [5, 0], [2, 5], [4, 0]]
       valid_ranges.each do |col1, col2|
         it 'returns true' do
           expect(bitmap.valid_column_range?(col1, col2)).to be true
@@ -78,9 +76,9 @@ RSpec.describe 'Bitmap' do
       end
     end
     context 'when the given range is out of bounds' do
-      invalid_lower_bounds = [[-1, -5], [-2, 0], [-1, 4], [0, 5]]
-      invalid_upper_bounds = [[1, 7], [15, 10]]
-      invalid_bounds = [[0, 7], [11, -11]]
+      invalid_lower_bounds = [[-1, -5], [-2, -1], [-1, 5]]
+      invalid_upper_bounds = [[0, 6], [15, 10]]
+      invalid_bounds = [[-1, 6], [11, -11]]
       invalid_ranges = invalid_lower_bounds + invalid_upper_bounds + invalid_bounds
       invalid_ranges.each do |col1, col2|
         it 'returns false' do
@@ -93,7 +91,7 @@ RSpec.describe 'Bitmap' do
   describe '#valid_row_range?' do
     bitmap = Bitmap.new(7, 8)
     context 'when the given range is within bounds' do
-      valid_ranges = [[1, 8], [1, 5], [4, 8], [6, 2]]
+      valid_ranges = [[0, 7], [0, 4], [3, 7], [5, 1]]
       valid_ranges.each do |row1, row2|
         it 'returns true' do
           expect(bitmap.valid_row_range?(row1, row2)).to be true
@@ -101,9 +99,9 @@ RSpec.describe 'Bitmap' do
       end
     end
     context 'when the given range is out of bounds' do
-      invalid_lower_bounds = [[-1, -5], [-2, 0], [-1, 4], [0, 5]]
-      invalid_upper_bounds = [[1, 9], [15, 10]]
-      invalid_bounds = [[0, 9], [11, -11]]
+      invalid_lower_bounds = [[-1, -5], [-2, 1], [-1, 4]]
+      invalid_upper_bounds = [[1, 8], [15, 10]]
+      invalid_bounds = [[-1, 8], [11, -11]]
       invalid_ranges = invalid_lower_bounds + invalid_upper_bounds + invalid_bounds
       invalid_ranges.each do |row1, row2|
         it 'returns false' do
@@ -116,9 +114,9 @@ RSpec.describe 'Bitmap' do
   describe '#set_pixel_color' do
     context 'when arguments are out of bounds' do
       bitmap = Bitmap.new(8, 9)
-      invalid_cols = [[0, 1, 1, 5, 'A'], [9, 1, 6, 3, 'A'], [0, 9, 9, 1, 'A'], [8, -1, 1, 9, 'A']]
-      invalid_rows = [[8, 8, 0, 1, 'A'], [8, 1, 10, 1, 'A'], [4, 7, 0, 10, 'A'], [5, 1, -1, 9, 'A']]
-      invalid_both = [[0, 1, 0, 1, 'A'], [-1, 1, 1, -1, 'A'], [9, -1, -1, 10, 'A'], [0, 5, 3, -2, 'A']]
+      invalid_cols = [[-1, 0, 0, 4, 'A'], [8, 0, 5, 2, 'A'], [-1, 8, 8, 0, 'A'], [7, -1, 0, 8, 'A']]
+      invalid_rows = [[7, 7, -1, 0, 'A'], [7, 0, 9, 0, 'A'], [3, 6, -1, 9, 'A'], [4, 0, -1, 8, 'A']]
+      invalid_both = [[-1, 0, -1, 0, 'A'], [-1, 0, 0, -1, 'A'], [8, -1, -1, 9, 'A'], [-1, 4, 2, -2, 'A']]
       invalid_args = invalid_cols + invalid_rows + invalid_both
       invalid_args.each do |c1, c2, r1, r2, color|
         it 'raises error' do
@@ -129,18 +127,18 @@ RSpec.describe 'Bitmap' do
     context 'when arguments are in bounds' do
       before(:each) { @bitmap = Bitmap.new(8, 9) }
       it 'set color for a single pixel when col1 == col2 and row1 == row2' do
-        @bitmap.set_pixel_color(1, 1, 1, 1, 'A')
+        @bitmap.set_pixel_color(0, 0, 0, 0, 'A')
         expect(@bitmap.get_pixel_color(0, 0)).to eq('A')
       end
       it 'set color for pixels on a vertical line when col1 == col2 and row1 != row2' do
-        @bitmap.set_pixel_color(2, 2, 1, 3, 'B')
+        @bitmap.set_pixel_color(1, 1, 0, 2, 'B')
         set_correctly = @bitmap.get_pixel_color(1, 0) == 'B'
         set_correctly &&= @bitmap.get_pixel_color(1, 1) == 'B'
         set_correctly &&= @bitmap.get_pixel_color(1, 2) == 'B'
         expect(set_correctly).to be true
       end
       it 'set color for pixels on a horizontal line when col1 != col2 and row1 == row2' do
-        @bitmap.set_pixel_color(4, 2, 5, 5, 'C')
+        @bitmap.set_pixel_color(3, 1, 4, 4, 'C')
         set_correctly = @bitmap.get_pixel_color(3, 4) == 'C'
         set_correctly &&= @bitmap.get_pixel_color(2, 4) == 'C'
         set_correctly &&= @bitmap.get_pixel_color(1, 4) == 'C'
